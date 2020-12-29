1ca7203fe6f49093543a5b087ee1f48a3cadb300
wagyourtail <wagyourtail@wagyourtail.xyz>
2020-12-29 10:10:12 -0700

    exception handling

--- src/main/java/xyz/wagyourtail/jsmacros/jython/language/impl/JythonLanguageDescription.java
+++ src/main/java/xyz/wagyourtail/jsmacros/jython/language/impl/JythonLanguageDescription.java
@@ -1,10 +1,12 @@
 package xyz.wagyourtail.jsmacros.jython.language.impl;
 
+import org.python.core.*;
 import org.python.util.PythonInterpreter;
 import xyz.wagyourtail.jsmacros.core.Core;
 import xyz.wagyourtail.jsmacros.core.config.ScriptTrigger;
 import xyz.wagyourtail.jsmacros.core.event.BaseEvent;
 import xyz.wagyourtail.jsmacros.core.language.BaseLanguage;
+import xyz.wagyourtail.jsmacros.core.language.BaseWrappedException;
 
 import java.io.File;
 import java.nio.file.Path;
@@ -43,6 +45,21 @@ public class JythonLanguageDescription extends BaseLanguage {
             
             interp.exec(script);
         });
+    }
+    
+    @Override
+    public BaseWrappedException<?> wrapException(Throwable ex) {
+        if (ex instanceof PyException) {
+            String message = ((PyType)((PyException) ex).type).fastGetName();
+            if (((PyException) ex).value instanceof PyString) message += " " + ((PyException) ex).value.asString();
+            return new BaseWrappedException<>(ex, message, null, wrapTraceback(((PyException) ex).traceback));
+        }
+        return null;
+    }
+    
+    public BaseWrappedException<?> wrapTraceback(PyTraceback traceback) {
+        BaseWrappedException.GuestLocation loc = new BaseWrappedException.GuestLocation(new File(traceback.tb_frame.f_code.co_filename), -1, -1, traceback.tb_lineno, -1);
+        return new BaseWrappedException<>(traceback, " at " + traceback.tb_frame.f_code.co_name, loc, traceback.tb_next != null ? wrapTraceback((PyTraceback) traceback.tb_next) : null);
     }
     
     protected interface Executor {
